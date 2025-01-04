5d948c5587a574f06b7dc384371a9c839ceee4da
Eltayeb Ibrahim <tayeb1293@yahoo.com>
2025-01-04 07:39:45 +0200

    utils: date formatter

---
+++ src/badrbillingsystem/utils/DateFormatter.java
@@ -0,0 +1,18 @@
+
+package badrbillingsystem.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateFormatter {
+    
+//    public static SimpleDateFormat df = new SimpleDateFormat("dd-MMMM-yyyy");
+    
+    DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    
+    public String format(LocalDateTime date) {
+        String formatedDate = date.format(df);
+        return formatedDate;
+    }
+    
+}
