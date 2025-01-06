d7f9cb1edfd2d0aa6995a3176788567bee4e0ee4
Eltayeb Ibrahim <tayeb1293@yahoo.com>
2025-01-06 15:06:57 +0200

    controller: sales invoice

--- src/badrbillingsystem/controller/SalesInvoiceController.java
+++ src/badrbillingsystem/controller/SalesInvoiceController.java
@@ -1,19 +1,25 @@
 
 package badrbillingsystem.controller;
 
+import badrbillingsystem.models.Customer;
 import badrbillingsystem.models.Product;
 import badrbillingsystem.models.SalesInvoiceDetails;
+import badrbillingsystem.repos.customer.CustomerRepo;
 import badrbillingsystem.repos.product.ProductRepo;
 import badrbillingsystem.utils.AlertMaker;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
@@ -23,12 +29,16 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.util.converter.DoubleStringConverter;
 
 
 public class SalesInvoiceController implements Initializable{
@@ -36,6 +46,17 @@ public class SalesInvoiceController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         fillProductsGrid();
+        tbInvocieDetails.setPlaceholder(new Label("لا توجد بيانات"));
+        setInvoiceDate();
+        fillCustomersComboBox();
+        cbCustomerName.focusedProperty().addListener(new ChangeListener<Boolean>(){
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                Customer c = customerRepo.findByName(cbCustomerName.getSelectionModel().getSelectedItem());
+                txtCutomerPhone.setText(c.getPhone());
+            }
+        
+        });
     }
     
     @FXML
@@ -87,6 +108,8 @@ public class SalesInvoiceController implements Initializable{
     private TextField txtTotal;
     
     ObservableList<SalesInvoiceDetails> data = FXCollections.observableArrayList();
+    DecimalFormat df = new DecimalFormat("#,###.##");
+    CustomerRepo customerRepo = new CustomerRepo();
 
     @FXML
     void newInvoice(ActionEvent event) {
@@ -161,22 +184,16 @@ public class SalesInvoiceController implements Initializable{
                 }
                 
                 addButton.setStyle("-fx-font-family: 'almarai';");
+                
+                
                 // action listener
                 
                 addButton.setOnAction(e -> {
-//                    AlertMaker.showMessageAlert(p.getName());
-                    System.out.println(p.getName());
                     SalesInvoiceDetails details = new SalesInvoiceDetails();
-//                    details.setDetails(details);
-//                    details.setHeaderId(details);
                     details.setPrice(p.getPrice());
                     details.setQuantity(0);
                     details.setProductName(p.getName());
                     details.setDetails("");
-//                    details.setDetails(details);
-//                    details.setDetails(details);
-//                    details.setDetails(details);
-//                    details.setDetails(details);
                     addProductToInvoiceTable(details);
                 });
                 
@@ -192,11 +209,137 @@ public class SalesInvoiceController implements Initializable{
     private void addProductToInvoiceTable(SalesInvoiceDetails details) {
         try {
             
+            // col details
+            colDetails.setCellValueFactory(new PropertyValueFactory<>("details"));
+            colDetails.setCellFactory(TextFieldTableCell.forTableColumn());
+            colDetails.setOnEditCommit(new EventHandler<CellEditEvent<SalesInvoiceDetails, String>>(){
+                @Override
+                public void handle(CellEditEvent<SalesInvoiceDetails, String> event) {
+                    SalesInvoiceDetails d = event.getRowValue();
+                    d.setDetails(event.getNewValue());
+                }
+            
+            });
+        
+            
+            // col price
+            colPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+            colPrice.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));
+            colPrice.setOnEditCommit(new EventHandler<CellEditEvent<SalesInvoiceDetails, Double>>() {
+                @Override
+                public void handle(CellEditEvent<SalesInvoiceDetails, Double> t) {
+                    SalesInvoiceDetails d = t.getRowValue();
+                    d.setPrice(t.getNewValue());
+                    double total = d.getPrice() * d.getQuantity();
+                    d.setTotal(total);
+                    tbInvocieDetails.refresh();
+                    calculateTotals();
+                }
+            });
+            
+            // col name
+            colProductName.setCellValueFactory(new PropertyValueFactory<>("productName"));
+            
+            // col quantity
+            colQuantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+            colQuantity.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));
+            colQuantity.setOnEditCommit(new EventHandler<CellEditEvent<SalesInvoiceDetails, Double>>() {
+                @Override
+                public void handle(CellEditEvent<SalesInvoiceDetails, Double> t) {
+                    SalesInvoiceDetails d = t.getRowValue();
+                    d.setQuantity(t.getNewValue());
+                    double total = d.getPrice() * d.getQuantity();
+                    d.setTotal(total);
+                    tbInvocieDetails.refresh();
+                    calculateTotals();
+                }
+            });
+            
+            // col total
+            colTotal.setCellValueFactory(new PropertyValueFactory<>("total"));
+            
+            for (SalesInvoiceDetails d : data) {
+                if(d.getProductName() == details.getProductName()){
+                    AlertMaker.showErrorALert("هذا المنتج تم اضافته سابقا . اختر عنصر اخر");
+                    return;
+                }
+            }
+            
+            data.add(details);
+            tbInvocieDetails.setItems(data);
         } catch (Exception e) {
             e.printStackTrace();
             AlertMaker.showErrorALert(e.toString());
         }
     }
     
+    @FXML
+    private void calculateTotals(){
+        try {
+            
+            if(txtDiscount.getText().isEmpty()){
+                txtDiscount.setText("0.0");
+            }
+            
+            if(txtTaxInPercentage.getText().isEmpty()){
+                txtTaxInPercentage.setText("0.0");
+            }
+            
+            double subtotal = 0;
+            for (SalesInvoiceDetails d : data) {
+                subtotal = subtotal + d.getTotal();
+            }
+            txtSubTotal.setText(df.format(subtotal));
+            
+            double discount = Double.valueOf(txtDiscount.getText());
+            double taxInPercentage = Double.valueOf(txtTaxInPercentage.getText());
+            double tax = subtotal * (taxInPercentage / 100);
+            txtTaxInNumber.setText(df.format(tax) + " ريال");
+            
+            double total = (subtotal + tax) - discount;
+            
+            txtTotal.setText(df.format(total));
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            AlertMaker.showErrorALert(e.toString());
+        }
+    }
     
+    @FXML 
+    private void deleteProductFromTable() {
+        try {
+            SalesInvoiceDetails d = tbInvocieDetails.getSelectionModel().getSelectedItem();
+            
+            data.remove(d);
+        } catch (Exception e) {
+            e.printStackTrace();
+            AlertMaker.showErrorALert(e.toString());
+        }
+    }
+
+    private void setInvoiceDate() {
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            AlertMaker.showErrorALert(e.toString());
+        }
+    }
+
+    private void fillCustomersComboBox() {
+        try {
+            ArrayList<Customer> customers = customerRepo.findAll();
+            ObservableList<String> data = FXCollections.observableArrayList();
+            
+            for(Customer c : customers) {
+                data.add(c.getName());
+            }
+            
+            cbCustomerName.setItems(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+            AlertMaker.showErrorALert(e.toString());
+        }
+    }
 }
