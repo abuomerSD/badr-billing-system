df2b262a9447c3e0ef95f6896226e55e5c7916b2
Eltayeb Ibrahim <tayeb1293@yahoo.com>
2025-01-07 16:45:01 +0200

    controller: return document

--- src/badrbillingsystem/controller/ReturnDocumentController.java
+++ src/badrbillingsystem/controller/ReturnDocumentController.java
@@ -1,6 +1,9 @@
 
 package badrbillingsystem.controller;
 
+import badrbillingsystem.models.ReturnDocumentDetails;
+import badrbillingsystem.models.ReturnDocumentHeader;
+import badrbillingsystem.utils.AlertMaker;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -8,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -18,8 +22,12 @@ public class ReturnDocumentController implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        tbReturnDocumentDetails.setPlaceholder(new Label("لا توجد بيانات في الجدول"));
+        tbReturnDocumentsList.setPlaceholder(new Label("لا توجد بيانات في الجدول"));
     }
+    
+    @FXML
+    TextField txtSalesInvoiceId;
     
     @FXML
     private Tab colProductName;
@@ -28,22 +36,22 @@ public class ReturnDocumentController implements Initializable{
     private DatePicker dpDate;
 
     @FXML
-    private TableView<?> tbReturnDocumentDetails;
+    private TableView<ReturnDocumentDetails> tbReturnDocumentDetails;
 
     @FXML
-    private TableColumn<?, ?> colPName;
+    private TableColumn<ReturnDocumentDetails, String> colPName;
 
     @FXML
-    private TableColumn<?, ?> colPQuantity;
+    private TableColumn<ReturnDocumentDetails, Double> colPQuantity;
 
     @FXML
-    private TableColumn<?, ?> colPPrice;
+    private TableColumn<ReturnDocumentDetails, Double> colPPrice;
 
     @FXML
-    private TableColumn<?, ?> colPTotal;
+    private TableColumn<ReturnDocumentDetails, Double> colPTotal;
 
     @FXML
-    private TableColumn<?, ?> colPDetails;
+    private TableColumn<ReturnDocumentDetails, String> colPDetails;
 
     @FXML
     private TextField txtDiscount;
@@ -55,56 +63,91 @@ public class ReturnDocumentController implements Initializable{
     private TextField txtTotal;
 
     @FXML
-    private ComboBox<?> cbFilterTableByCustomerName;
+    private ComboBox<String> cbFilterTableByCustomerName;
 
     @FXML
-    private TableView<?> tbReturnDocumentsList;
+    private TableView<ReturnDocumentHeader> tbReturnDocumentsList;
 
     @FXML
-    private TableColumn<?, ?> colListDate;
+    private TableColumn<ReturnDocumentHeader, String> colListDate;
 
     @FXML
-    private TableColumn<?, ?> colListId;
+    private TableColumn<ReturnDocumentHeader, Long> colListId;
 
     @FXML
-    private TableColumn<?, ?> colListInvoiceId;
+    private TableColumn<ReturnDocumentHeader, Long> colListInvoiceId;
 
     @FXML
-    private TableColumn<?, ?> colListCustomerName;
+    private TableColumn<ReturnDocumentHeader, String> colListCustomerName;
 
     @FXML
-    private TableColumn<?, ?> colListDetails;
+    private TableColumn<ReturnDocumentHeader, String> colListDetails;
 
     @FXML
-    private TableColumn<?, ?> colListTotal;
+    private TableColumn<ReturnDocumentHeader, Double> colListTotal;
 
     @FXML
     void deleteReturnDocumentFromList(ActionEvent event) {
-
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            AlertMaker.showErrorALert(e.toString());
+        }
     }
 
     @FXML
     void newReturnDocument(ActionEvent event) {
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            AlertMaker.showErrorALert(e.toString());
+        }
 
     }
 
     @FXML
     void printReturnDocument(ActionEvent event) {
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            AlertMaker.showErrorALert(e.toString());
+        }
 
     }
 
     @FXML
     void printReturnDocumentFromList(ActionEvent event) {
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            AlertMaker.showErrorALert(e.toString());
+        }
 
     }
 
     @FXML
     void refreshListTable(ActionEvent event) {
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            AlertMaker.showErrorALert(e.toString());
+        }
 
     }
 
     @FXML
     void saveReturnDocument(ActionEvent event) {
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            AlertMaker.showErrorALert(e.toString());
+        }
 
     }
     
